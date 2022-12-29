c242e77bcc41c13b32817d35860234c1b8c1778e
João Damaceno <joaodamaceno188@gmail.com>
2022-12-29 17:32:41 -0400

    refactor: :recycle: Escurece imagem do header

--- resources/views/livewire/blog/show-blog.blade.php
+++ resources/views/livewire/blog/show-blog.blade.php
@@ -1,10 +1,10 @@
 <div class="">
-   <div class="w-full bg-cover bg-center bg-header-image" style="height:32rem;">
+   <div class="w-full bg-cover bg-center bg-header-image filter bg-blend-multiply" style="height:32rem;">
         <div clss="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-90">
-            <div class="text-center py-44">
-                <h1 class="text-white text-2xl font-extrabold uppercase md:text-3xl">
+            <div class="text-center py-44 ">
+                <h1 class="text-gray-200 text-2xl font-semibold uppercase md:text-3xl">
                     Bem-vindo ao meu
-                    <span class="underline text-blue-400">Blog</span>
+                    <span class="underline text-blue-500 font-semibold">Blog</span>
                 </h1>
                 <button class="mt-4 px-4 py-2 mr-5 bg-black text-white text-sm uppercase font-medium rounded 
                 hover:bg-gray-500 focus:bg-gray-500"><i class="fa-brands fa-github"></i> Github</button>
