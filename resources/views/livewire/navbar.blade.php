6677e6976840d77c408137ee7326684adf155c94
João Damaceno <joao.damaceno@j2aconsultoria.com.br>
2023-01-02 11:42:29 -0400

    refactor: :recycle: Adiciona rota para o blog.index na navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -10,7 +10,7 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent
+              <a href="{{route('blog.index')}}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent
                md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
             </li>
             <li>
