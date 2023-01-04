dbb0e766c538f80d263cfef5832d3bb6f50a68d1
João Damaceno <joao.damaceno@j2aconsultoria.com.br>
2023-01-04 16:47:58 -0400

    refactor: :recycle: Adiciona campo unico para o Slug

--- database/migrations/2022_12_05_040957_create_posts_table.php
+++ database/migrations/2022_12_05_040957_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('slug')->nullable();
+            $table->string('slug')->unique();
             $table->string('tema');
             $table->string('title');
             $table->string('description', 255);
